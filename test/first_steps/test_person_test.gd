7ce5abdb31f126d6b43fac9f78730989bec3ad68
Richard Ram <richard@richard-ram.com>
2024-07-05 12:20:13 +0200

    removed GUT, added GdUnit4

---
+++ test/first_steps/test_person_test.gd
@@ -0,0 +1,14 @@
+# GdUnit generated TestSuite
+class_name TestPersionTest
+extends GdUnitTestSuite
+@warning_ignore('unused_parameter')
+@warning_ignore('return_value_discarded')
+
+# TestSuite generated from
+const __source = 'res://first_steps/test_person.gd'
+
+
+func test_full_name() -> void:
+	var person := TestPersion.new("King", "Arthur")
+	assert_str(person.full_name()).is_equal("King Arthur")
+	person.free()
