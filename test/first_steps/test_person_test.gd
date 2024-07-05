1d0afcbc1fd3ccb9b9f6745230fde86edc079747
Richard Ram <richard@richard-ram.com>
2024-07-05 12:44:01 +0200

    fixed the test

--- test/first_steps/test_person_test.gd
+++ test/first_steps/test_person_test.gd
@@ -17,5 +17,5 @@ func test_full_name() -> void:
 func test_sum() -> void:
 	var person := TestPersion.new("King", "Arthur")
 	var sum = person.sum(1, 1)
-	assert_int(sum).is_equal(3)
+	assert_int(sum).is_equal(2)
 	person.free()
